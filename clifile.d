1f38b9e00ce4e074988ffabf506bfdfcc1daf781
Vladimir Panteleev <git@thecybershadow.net>
2017-02-24 11:54:56 +0000

    First implementation of deserialization (parsing)

--- clifile.d
+++ clifile.d
@@ -1,9 +1,11 @@
 module clifile;
 
+import std.algorithm.searching;
 import std.ascii;
 import std.base64;
 import std.conv;
 import std.datetime;
+import std.exception;
 import std.range;
 import std.string;
 import std.traits;
@@ -11,6 +13,7 @@ import std.traits;
 import ae.sys.windows.imports;
 import ae.utils.array;
 import ae.utils.time.format;
+import ae.utils.time.parse;
 
 mixin(importWin32!(q{winnt}));
 
@@ -66,6 +69,34 @@ immutable IMAGE_NT_HEADERS32 cliPEHeader =
 
 import ae.sys.windows.pe.pe;
 
+auto clone(T)(ref T var)
+{
+	static if (!hasIndirections!T)
+	{
+		Unqual!T result = var;
+		return result;
+	}
+	else
+	static if (is(T == struct))
+	{
+		Unqual!T result;
+		foreach (i, ref f; var.tupleof)
+			result.tupleof[i] = var.tupleof[i].clone();
+		return result;
+	}
+	else
+	static if (isDynamicArray!T)
+	{
+		alias E = Unqual!(typeof(var[0]));
+		E[] result = new E[var.length];
+		foreach (i, ref f; var)
+			result[i] = f.clone();
+		return result;
+	}
+	else
+		static assert(false, "Don't know how to clone " ~ T.stringof);
+}
+
 struct CLIFile
 {
 	struct Header
@@ -73,7 +104,7 @@ struct CLIFile
 		IMAGE_DOS_HEADER dosHeader = cliDosHeader;
 		ubyte[] dosStub = cliDosStub;
 		IMAGE_NT_HEADERS32 peHeader = cliPEHeader;
-		IMAGE_DATA_DIRECTORY[] dataDirectories;
+		IMAGE_DATA_DIRECTORY[] dataDirectories = new IMAGE_DATA_DIRECTORY[16];
 		IMAGE_SECTION_HEADER[] sections;
 
 		struct ImportFunction
@@ -147,42 +178,73 @@ struct CLIFile
 	}
 }
 
+T parseIntLiteral(T)(string s)
+{
+	if (s.skipOver("0x"))
+		return s.to!T(16);
+	else
+	if (s.skipOver("0b"))
+		return s.to!T(2);
+	else
+		return s.to!T();
+}
+
 struct DefaultSerializer
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		d.putVar!(typeof(field))(field, def);
+	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		return a.readVar!T(def);
 	}
 }
 
 struct HexIntegerSerializer
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		static assert(is(F : long));
 		d.writer.putValue("0x%X".format(field));
+	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		return a.readVar!T(def);
 	}
 }
 
 /// For zero-terminated strings in fixed-length arrays.
 struct CStrArrSerializer
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		auto arr = field[];
-		while (arr.length && arr[$-1] == 0)
+		while (arr.length && arr[$-1] == def[arr.length-1])
 			arr = arr[0..$-1];
 		d.writer.putString(cast(char[])arr);
+	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		auto s = a.reader.readString();
+		T result = def;
+		enforce(s.length <= result.length, "String too long");
+		foreach (i, c; s)
+			result[i] = c;
+		return result;
 	}
 }
 
 /// Constants which are not declared as an actual enum.
 struct ImplicitEnumSerializer(members...)
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		static assert(is(F : long));
@@ -198,6 +260,28 @@ struct ImplicitEnumSerializer(members...)
 				d.writer.putValue("%d".format(field));
 		}
 	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		auto word = a.reader.readWord();
+		a.reader.endNode();
+
+		if (word[0].isDigit())
+			return parseIntLiteral!T(word);
+		else
+		{
+			switch (word)
+			{
+				foreach (i, member; members)
+				{
+					case __traits(identifier, members[i]):
+						return member;
+				}
+				default:
+					throw new Exception("Unknown bitmask value: %s", word);
+			}
+		}
+	}
 }
 
 /// An array which might as well be a struct with all fields of the same type.
@@ -205,7 +289,7 @@ struct ImplicitEnumSerializer(members...)
 /// default values are omitted), and must be fixed or specified elsewhere.
 struct SparseNamedIndexedArraySerializer(members...)
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		d.writer.beginStruct();
@@ -214,14 +298,14 @@ struct SparseNamedIndexedArraySerializer(members...)
 			auto aDef = i < def.length ? def[i] : typeof(a).init;
 			if (a == aDef)
 				continue;
-		label:
+		memberSwitch:
 			switch (i)
 			{
 				foreach (memberIndex, member; members)
 				{
 					case member:
 						d.writer.beginTag(__traits(identifier, members[memberIndex]));
-						break label;
+						break memberSwitch;
 				}
 				default:
 					d.writer.beginTag(text(i));
@@ -231,12 +315,38 @@ struct SparseNamedIndexedArraySerializer(members...)
 		}
 		d.writer.endStruct();
 	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		a.reader.beginStruct();
+		T result = def.clone();
+		alias E = typeof(result[0]);
+		while (!a.reader.skipEndStruct())
+		{
+			auto tag = a.reader.readTag();
+		memberSwitch:
+			switch (tag)
+			{
+				foreach (i, member; members)
+				{
+					enum name = __traits(identifier, members[i]);
+					case name:
+						enforce(result.length > member, "%s array too small to fit member %s".format(T.stringof, name));
+						result[member] = a.readVar!E(member < def.length ? def[member] : initOf!E);
+						break memberSwitch;
+				}
+				default:
+					throw new Exception("Unknown array index constant: %s", tag);
+			}
+		}
+		return result;
+	}
 }
 
 /// Bitmask using constants which are not declared as an actual enum.
 struct ImplicitEnumBitmaskSerializer(members...)
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		static assert(is(F : long));
@@ -251,17 +361,54 @@ struct ImplicitEnumBitmaskSerializer(members...)
 			if (remainingBits & mask)
 				d.writer.putValue("0x%x".format(mask));
 	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		T result;
+		while (!a.reader.skipEndNode())
+		{
+			auto word = a.reader.readWord();
+			if (word[0].isDigit())
+				result |= parseIntLiteral!T(word);
+			else
+			{
+			memberSwitch:
+				switch (word)
+				{
+					foreach (i, member; members)
+					{
+						case __traits(identifier, members[i]):
+							result |= member;
+							break memberSwitch;
+					}
+					default:
+						throw new Exception("Unknown bitmask value: %s".format(word));
+				}
+			}
+		}
+		return result;
+	}
 }
 
 /// Unix timestamp serializer.
 struct UnixTimestampSerializer
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	enum timeFormat = "Y-m-d H:i:s";
+
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		static assert(is(F : long));
 		SysTime time = SysTime.fromUnixTime(field);
-		d.writer.putValue(time.formatTime!"Y-m-d H:i:s");
+		d.writer.putValue(time.formatTime!timeFormat);
+	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		auto dateStr = a.reader.readWord();
+		auto timeStr = a.reader.readWord();
+		a.reader.endNode();
+		return (dateStr ~ " " ~ timeStr).parseTime!timeFormat.toUnixTime.to!T();
 	}
 }
 
@@ -269,7 +416,7 @@ struct UnixTimestampSerializer
 /// field we will be looking at.
 struct UnionSerializer(uint fieldIndex)
 {
-	void putValue(F, D)(Disassembler d, in ref F field, in ref D def)
+	void putValue(F, D)(ref Disassembler d, in ref F field, in ref D def)
 		if (is(typeof(field) == typeof(def)))
 	{
 		d.writer.beginStruct();
@@ -282,6 +429,20 @@ struct UnionSerializer(uint fieldIndex)
 				d.writer.endTag();
 			}
 		d.writer.endStruct();
+	}
+
+	T readValue(T)(ref Assembler a, in ref T def)
+	{
+		a.reader.beginStruct();
+		T result;
+		foreach (i, ref f; result.tupleof)
+			static if (i == fieldIndex)
+			{
+				a.reader.expectTag(__traits(identifier, result.tupleof[i]));
+				f = a.readVar!(typeof(f))(def.tupleof[i]);
+			}
+		a.reader.endStruct();
+		return result;
 	}
 }
 
@@ -509,13 +670,18 @@ private:
 	}
 }
 
+bool isTagChar(char c)
+{
+	return isAlphaNum(c) || c.isOneOf("._");
+}
+
 struct Writer
 {
 	void beginTag(string s)
 	{
 		putIndent();
 		debug foreach (char c; s)
-			assert(isAlphaNum(c) || c.isOneOf("._"), "Bad tag name: " ~ s);
+			assert(isTagChar(c), "Bad tag name: " ~ s);
 		buf.put(s);
 	}
 
@@ -594,10 +760,396 @@ private:
 	Appender!string buf;
 }
 
+struct Assembler
+{
+	this(string src)
+	{
+		reader = Reader(src);
+	}
+
+	CLIFile assemble()
+	{
+		try
+		{
+			reader.expectTag("file");
+			return readVar!CLIFile();
+		}
+		catch (Exception e)
+		{
+			throw new Exception("Error at %d,%d".format(reader.tokenizer.line, reader.tokenizer.column), e);
+		}
+	}
+
+private:
+	Reader reader;
+
+	T readVar(T)(in ref T def = initOf!T)
+	{
+		static if (is(T == struct))
+		{
+			reader.beginStruct();
+			T var = def.clone();
+			while (!reader.skipEndStruct())
+			{
+				auto name = reader.readTag();
+			structSwitch:
+				switch (name)
+				{
+					foreach (i, ref f; var.tupleof)
+					{
+						enum fieldName = __traits(identifier, var.tupleof[i]);
+						case fieldName:
+						{
+							static assert(is(typeof(f = f)), typeof(f).stringof);
+							var.tupleof[i] = getSerializer!(T, fieldName).readValue!(typeof(f))(this, def.tupleof[i]);
+							break structSwitch;
+						}
+					}
+					default:
+						throw new Exception("Unknown %s field: %s".format(T.stringof, name));
+				}
+			}
+			reader.endNode();
+			return var;
+		}
+		else
+		static if (is(T == union))
+		{
+			static assert(false, "Can't unserialize a union: " ~ T.stringof);
+		}
+		else
+		static if (is(T : ulong))
+		{
+			auto value = reader.readWord().parseIntLiteral!T();
+			reader.endNode();
+			return value;
+		}
+		else
+		static if (is(T == string))
+		{
+			auto value = reader.readString();
+			reader.endNode();
+			return value;
+		}
+		else
+		static if (is(T : const(ubyte)[]))
+		{
+			reader.beginData();
+			T result;
+			while (!reader.skipEndData())
+				result ~= Base64.decode(reader.readData());
+			reader.endNode();
+			return result;
+		}
+		else
+		static if (is(T : A[n], A, size_t n))
+		{
+			reader.beginStruct();
+			T result;
+			foreach (i, ref f; result)
+			{
+				enforce(reader.readTag() == Unqual!A.stringof, "%s tag expected".format(Unqual!A.stringof));
+				f = readVar!A(def[i]);
+			}
+			reader.endStruct();
+			reader.endNode();
+			return result;
+		}
+		else
+		static if (is(T A : A[]))
+		{
+			reader.beginStruct();
+			T result;
+			while (!reader.skipEndStruct())
+			{
+				enforce(reader.readTag() == Unqual!A.stringof, "%s tag expected".format(Unqual!A.stringof));
+				const A aDef = result.length < def.length ? def[result.length] : A.init;
+				result ~= readVar!A(aDef);
+			}
+			reader.endNode();
+			return result;
+		}
+		else
+			static assert(false, "Don't know how to put " ~ T.stringof);
+	}
+}
+
+/// SDLang parser (on top of tokenizer).
+struct Reader
+{
+	@disable this();
+
+	this(string s)
+	{
+		tokenizer = Tokenizer(s);
+	}
+
+	/// Return the name of the next tag.
+	/// Throws if the next object is not a tag.
+	string readTag()
+	{
+		skipDelims();
+		return readWord();
+	}
+
+	/// If the next token is a tag with the given name, skip over it. Throw otherwise.
+	void expectTag(string name)
+	{
+		auto tag = readTag();
+		enforce(tag == name, "'%s' expected, not '%s'".format(name, tag));
+	}
+
+	/// Return the next word as a string.
+	string readWord()
+	{
+		return expect(Tokenizer.Token.Type.word).value;
+	}
+
+	/// Skip past a node delimiter. Throws otherwise.
+	void endNode()
+	{
+		expect(Tokenizer.Token.Type.nodeDelim);
+	}
+
+	/// If the next token is a node delimiter, return true and skip it.
+	bool skipEndNode()
+	{
+		return skipOver(Tokenizer.Token.Type.nodeDelim);
+	}
+
+	/// Skip past a {. Throws otherwise.
+	void beginStruct()
+	{
+		expect(Tokenizer.Token.Type.beginStruct);
+	}
+
+	/// Skip past a }. Throws otherwise.
+	void endStruct()
+	{
+		expect(Tokenizer.Token.Type.endStruct);
+	}
+
+	/// If the next token is }, return true and skip it.
+	bool skipEndStruct()
+	{
+		skipDelims();
+		return skipOver(Tokenizer.Token.Type.endStruct);
+	}
+
+	/// Return the value of the next string literal.
+	/// Throws if the next object is not a string literal.
+	string readString()
+	{
+		return expect(Tokenizer.Token.Type.string).value;
+	}
+
+	/// Skip past a [. Throws otherwise.
+	void beginData()
+	{
+		expect(Tokenizer.Token.Type.beginData);
+	}
+
+	/// Read and return the next Base64 data block.
+	alias readData = readTag;
+
+	/// If the next token is ], return true and skip it.
+	bool skipEndData()
+	{
+		skipDelims();
+		return skipOver(Tokenizer.Token.Type.endData);
+	}
+
+private:
+	Tokenizer tokenizer;
+
+	/// Skip over node delimiters.
+	void skipDelims()
+	{
+		while (tokenizer.front.type == Tokenizer.Token.Type.nodeDelim)
+			tokenizer.popFront();
+	}
+
+	/// Consume a token of the indicated type and return it, throw otherwise.
+	Tokenizer.Token expect(Tokenizer.Token.Type type)
+	{
+		enforce(tokenizer.front.type == type, "%s expected, not %s".format(type, tokenizer.front));
+		auto token = tokenizer.front;
+		tokenizer.popFront();
+		return token;
+	}
+
+	/// If the next token is of the given type, return true and skip over it.
+	bool skipOver(Tokenizer.Token.Type type)
+	{
+		if (tokenizer.front.type == type)
+		{
+			tokenizer.popFront();
+			return true;
+		}
+		return false;
+	}
+}
+
+/// SDLang tokenizer.
+struct Tokenizer
+{
+	@disable this();
+	@disable this(this);
+
+	struct Token
+	{
+		enum Type
+		{
+			word,               // Tag name, number, enum value, data block
+			beginStruct,        // {
+			endStruct,          // }
+			nodeDelim,          // Newline or ;
+			string,             // Quoted string
+			beginData,          // [
+			endData,            // ]
+			eof,
+		}
+		Type type;
+
+		string value;           // For word and string
+	}
+
+	this(string s)
+	{
+		this.s = s;
+		line = column = 1;
+		popFront();
+	}
+
+	Token front;
+	bool empty = false;
+
+	void popFront()
+	{
+		while (s.length && (s[0] == ' ' || s[0] == '\t'))
+			advance();
+
+		if (!s.length)
+		{
+			front = Token(Token.Type.eof);
+			empty = true;
+			return;
+		}
+
+		switch (s[0])
+		{
+			case '\n':
+			case '\r':
+			case ';':
+				front = Token(Token.Type.nodeDelim);
+				advance();
+				return;
+			case '{':
+				front = Token(Token.Type.beginStruct);
+				advance();
+				return;
+			case '}':
+				front = Token(Token.Type.endStruct);
+				advance();
+				return;
+			case '[':
+				front = Token(Token.Type.beginData);
+				advance();
+				return;
+			case ']':
+				front = Token(Token.Type.endData);
+				advance();
+				return;
+			case '"':
+				front = Token(Token.Type.string, readString());
+				return;
+			default:
+				front = Token(Token.Type.word, readWord());
+				enforce(front.value.length, "Syntax error: Invalid character " ~ s[0]);
+				return;
+		}
+	}
+
+private:
+	string s;
+	int line, column;
+
+	void advance(size_t num = 1)
+	{
+		foreach (char c; s[0..num])
+			if (c == '\n')
+			{
+				line++;
+				column = 1;
+			}
+			else
+				column++;
+		s = s[num..$];
+	}
+
+	static bool isWordChar(char c)
+	{
+		return isTagChar(c)
+			|| c == '/' || c == '+' || c == '=' // Base64 data
+			|| c == '-' || c == ':'             // Timestamps
+		;
+	}
+
+	string readString()
+	{
+		// TODO: WYSIWYG strings
+		assert(s.length && s[0] == '"');
+		advance();
+		string result;
+		size_t p = 0;
+		while (true)
+		{
+			enforce(s.length, "Unexpected EOF within string literal");
+			switch (s[p])
+			{
+				case '"':
+					if (result.length)
+						result ~= s[0..p];
+					else
+						result = s[0..p];
+					advance(p+1);
+					return result;
+				case '\\':
+					if (result.length)
+						result ~= s[0..p];
+					else
+						result = s[0..p];
+					advance(p+1);
+					p = 0;
+					break;
+				default:
+					p++;
+					break;
+			}
+		}
+	}
+
+	string readWord()
+	{
+		auto p = 0;
+		while (p < s.length && isWordChar(s[p]))
+			p++;
+		auto result = s[0..p];
+		advance(p);
+		return result;
+	}
+}
+
 void main()
 {
 	import std.file;
 	auto cli = CLIFile(cast(ubyte[])read("test.exe"));
 	auto disassembler = Disassembler(&cli);
-	write("test.rcli", disassembler.disassemble());
+	auto disassembly = disassembler.disassemble();
+	write("test.rcli", disassembly);
+	auto assembler = Assembler(disassembly);
+	auto cli2 = assembler.assemble();
+	auto disassembler2 = Disassembler(&cli2);
+	auto disassembly2 = disassembler2.disassemble();
+	write("test2.rcli", disassembly2);
 }
